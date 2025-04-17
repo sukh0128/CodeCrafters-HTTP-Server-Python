a154d197f221123eb65c50a51f0a635e67027ea9
Eric Tsendjav <erdenesukh@gmail.com>
2025-04-16 20:59:55 -0600

    added path/resource check for HTTP server

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-
+    conn, addr = server_socket.accept()    
+    data = conn.recv(1024).decode().split(" ")
+    if data[1] == "/":
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 if __name__ == "__main__":
     main()
