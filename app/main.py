7a373f3284b2f301e458aa590615b08c66f585ed
Eric Tsendjav <erdenesukh@gmail.com>
2025-04-17 20:01:36 -0600

    added echo/string endpoint

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept()    
     data = conn.recv(1024).decode().split(" ")
-    if data[1] == "/":
-        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    if data[1].startswith("/echo/"):
+        body = data[1].split("/")[2]
+        response = f"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: {len(body)}\r\n{body}"
+        conn.sendall(response.encode())
     else:
         conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        
 if __name__ == "__main__":
     main()
